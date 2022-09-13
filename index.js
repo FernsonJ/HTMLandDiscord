3018ba33f77b2acacd8a7d64401138e81b5c7374
FernsonJ <112515169+FernsonJ@users.noreply.github.com>
2022-09-13 12:12:04 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+//This is only a Testing File with HTML. I am not fully learned with JavaScript but if you do a seprated file with this, It would be easier for JavaScript. It's Basically Discord.js
+const discord = require("discord.js");
+const config = require('./config.json')
+const keepAlive = require('./server.js');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+keepAlive();
